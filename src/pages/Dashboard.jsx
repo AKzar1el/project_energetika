30632b12b4ef6578f993913793673ac215890800
AKzar1el <tomi.seregi99@gmail.com>
2022-11-15 12:10:07 +0100

    checkpoint 1.0

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,151 @@
+import React from 'react';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+
+import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
+import { dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
+
+const DropDown = ({ currentMode }) => (
+  <div className="w-28 border-1 border-color px-2 py-1 rounded-md">
+    <DropDownListComponent id="time" fields={{ text: 'Time', value: 'Id' }} style={{ border: 'none', color: (currentMode === 'Dark') && 'white' }} value="1" dataSource={dropdownData} popupHeight="220px" popupWidth="120px" />
+  </div>
+);
+
+const Dashboard = () => {
+  const { currentColor, currentMode } = useStateContext();
+
+  return (
+    <div className="mt-24">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="font-bold text-gray-400">Iskalnik</p>
+              <p className="text-2xl">Leta</p>
+            </div>
+          </div>
+          <div className="mt-6">
+            <Button
+              color="white"
+              bgColor={currentColor}
+              text="Išči"
+              borderRadius="10px"
+            />
+          </div>
+        </div>
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+            <div className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl ">
+              <p className="mt-3">
+                <span className="text-lg font-semibold">Check boxi</span>
+              </p>
+              <p>
+                Proizvodnja na pragu
+              </p>
+              <p>
+                Proizvodnja na generatorju
+              </p>
+            </div>
+        </div>
+      </div>
+
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Grafikoni</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="">
+              <div className="mt-5">
+                <SparkLine currentColor={currentColor} id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color={currentColor} />
+              </div>
+            </div>
+            <div>
+              <Stacked currentMode={currentMode} width="320px" height="360px" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className=" rounded-2xl md:w-400 p-4 m-3"
+            style={{ backgroundColor: currentColor }}
+          >
+            <div className="flex justify-between items-center ">
+              <p className="font-semibold text-white text-2xl">Skupaj:</p>
+
+              <div>
+                <p className="text-2xl text-white font-semibold mt-8">63,448.78</p>
+                <p className="text-gray-200">(1000 T)</p>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <SparkLine currentColor={currentColor} id="column-sparkLine" height="100px" type="Column" data={SparklineAreaData} width="320" color="rgb(242, 252, 253)" />
+            </div>
+          </div>
+
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl md:w-400 p-8 m-3 flex justify-center items-center gap-10">
+            <div>
+              <p className="text-2xl font-semibold ">Skupaj: 43,246</p>
+              <p className="text-gray-400">Grafikon 2.</p>
+            </div>
+
+            <div className="w-40">
+              <Pie id="pie-chart" data={ecomPieChartData} legendVisiblity={false} height="160px" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex gap-10 m-4 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl">
+          <div className="flex justify-between items-center gap-2">
+            <p className="text-xl font-semibold">Recent Transactions</p>
+          </div>
+          <div className="mt-10 w-72 md:w-400">
+            <h1>
+              Pregled statistike
+            </h1>
+          </div>
+          <div className="flex justify-between items-center mt-5 border-t-1 border-color">
+            <div className="mt-3">
+              <Button
+                color="white"
+                bgColor={currentColor}
+                text="Naloži generirano vsebino"
+                borderRadius="10px"
+              />
+            </div>
+
+            <p className="text-gray-400 text-sm">36 Recent Transactions</p>
+          </div>
+        </div>
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl w-96 md:w-760">
+          <div className="flex justify-between items-center gap-2 mb-10">
+            <p className="text-xl font-semibold">Sales Overview</p>
+            <DropDown currentMode={currentMode} />
+          </div>
+          <div className="md:w-full overflow-auto">
+            <LineChart />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+export { default as Dashboard } from "./Dashboard";
+export { default as Area } from "./Charts/Area";
+export { default as Bar } from "./Charts/Bar";
+export { default as ColorMapping } from "./Charts/ColorMapping";
+export { default as Financial } from "./Charts/Financial";
+export { default as Line } from "./Charts/Line";
+export { default as Pie } from "./Charts/Pie";
+export { default as Pyramid } from "./Charts/Pyramid";
+export { default as Stacked } from "./Charts/Stacked";
